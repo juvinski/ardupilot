a93f6a19b91d6aed21cb3eaca996b4de211b8c83
Vinicius Juvinski <juvinski@hotmail.com>
2021-01-24 00:41:03 -0300

    AP_HAL_Linux: SPIDevice: Add support for BBAI Board 	Add support for BBAI board - using BBBMINI cape board.

--- libraries/AP_HAL_Linux/SPIDevice.cpp
+++ libraries/AP_HAL_Linux/SPIDevice.cpp
@@ -145,6 +145,12 @@ SPIDesc SPIDeviceManager::_device[] = {
     SPIDesc("rst_a",    0, 2,  SPI_MODE_3, 8, SPI_CS_KERNEL,  1*MHZ, 10*MHZ),
     SPIDesc("ms5611",   0, 3,  SPI_MODE_3, 8, SPI_CS_KERNEL,  1*MHZ, 10*MHZ),
 };
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBAI
+SPIDesc SPIDeviceManager::_device[] = {
+    SPIDesc("mpu9250",    2, 0, SPI_MODE_3, 8, SPI_CS_KERNEL,  1*MHZ, 11*MHZ),
+    SPIDesc("mpu9250ext", 1, 0, SPI_MODE_3, 8, SPI_CS_KERNEL,  1*MHZ, 11*MHZ),
+    SPIDesc("ms5611",     2, 1, SPI_MODE_3, 8, SPI_CS_KERNEL,  10*MHZ,10*MHZ),
+};
 #else
 // empty device table
 SPIDesc SPIDeviceManager::_device[] = {
